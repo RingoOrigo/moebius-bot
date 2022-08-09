a34b3bbec244ef2354e7ba6b0a86043143f6cadd
Ringo Origo <59732670+RingoOrigo@users.noreply.github.com>
2022-08-09 11:19:30 -0400

    Rich Presence status cycle, global slash commands

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import discord
 import random
 import os
 
-from discord import SlashCommandGroup
+from discord import SlashCommandGroup, Activity
 from discord.ext import tasks, commands
 from itertools import cycle
 
@@ -12,7 +12,7 @@ intents.message_content = True
 #Open the token.txt file (not present in this repository) to obtain the bot's token to enable it to run.
 token = open("token.txt", "r").read()
 bot = discord.Bot(intents = intents)
-statusList = ["Xenoblade Chronicles 3", "The Eternal Now", "The Cycle of Rebirth"]
+statusList = ["Xenoblade Chronicles 3", "The Eternal Now", "The Cycle of Rebirth", "with your lives."]
 testGuildList = [927588913466462269, 971409630234296371, 824745963418812476]
 #Play event when bot is ready to be used.
 @bot.event
@@ -29,11 +29,21 @@ async def on_message(message):
         print("Get moebed lmao")
         await message.reply("It's Moebin' time!", mention_author=False)
 
-@bot.slash_command(guild_ids = testGuildList)
+@tasks.loop(minutes=7)
+async def changePresence():
+    status = next(bot.statuses)
+    await bot.change_presence(activity=discord.Game(name=status))
+
+@changePresence.before_loop
+async def waitForReady():
+    await bot.wait_until_ready()
+    bot.statuses = cycle(statusList)
+        
+@bot.slash_command()
 async def hello(ctx):
     await ctx.respond("Hello, Ouroboros.")
 
-@bot.slash_command(guild_ids = testGuildList)
+@bot.slash_command()
 async def roulette(message):
         i = random.randrange(1, 50)
         author = message.author.display_name
@@ -44,7 +54,7 @@ async def roulette(message):
             return
         await message.respond(f":boom: " + random.choice(deathMessages) + ", losing the roulette.")
 
-#Automatically loop through all files in cog directory and make the bot recognise them
+#Automatically loop through all files in cog directory and make the bot recognise them (Currently commented out because no cogs exist.)
 #for cog in os.listdir("./cogs"):
 #    if cog.endswith(".py"):
 #        print(f"Found {cog}!")
@@ -52,4 +62,5 @@ async def roulette(message):
 #        bot.load_extension(cog)
 #        print(f"Loaded {cog} successfully!")
 
+changePresence.start()
 bot.run(token)
