e76fabf22bce115ac889551fdc430e6bb9f498bd
Ringo Origo <59732670+RingoOrigo@users.noreply.github.com>
2022-08-10 13:07:45 -0400

    Add files via upload

---
+++ cogs/memes.py
@@ -0,0 +1,26 @@
+import discord
+import random
+
+from discord.ext import commands
+from config import config
+
+class memes(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+
+	@commands.slash_command()
+	async def moeb(self, ctx: discord.ApplicationContext):
+		await ctx.respond("IT'S MOEBIN' TIME!")
+
+	@commands.slash_command()
+	async def roulette(self, message):
+		i = random.randrange(1, 50)
+		author = message.author.display_name
+		if i == 7:
+			await message.respond(f":gun: {author} has survived the roulette... This time.")
+			await message.respond(f"You won't be so lucky next time, {author}.", ephemeral=True)
+			return
+		await message.respond(f":boom: {author} " + random.choice(config.ROULETTE_DEATH_MESSAGES) + ", losing the roulette.")
+
+def setup(bot):
+	bot.add_cog(memes(bot))
---
+++ config/config.py
@@ -0,0 +1,21 @@
+BOT_TOKEN: str = "Place your token here, in these quotes."
+
+TEST_GUILDS = [an, 
+				array, 
+				of, 
+				guid IDs]
+
+STATUS_LIST = ["An", 
+				"Array", 
+				"Of", 
+				"Potential",
+				"Statuses",]
+
+ROULETTE_DEATH_MESSAGES = ["has blown their own brains out", 
+							"has tripped and fallen onto the trigger", 
+							"won the roulette initially, then got confident and proceeded to try again and blow their own brains out", 
+							"died by their own hand"]
+
+START_MESSAGE: str = f"Logging in...\nSuccess!\nCalculating version... Done!\n (Your bot's name) Version: (version)"
+COG_LOAD: str = f"Loaded cog"
+COG_SUCCESS: str = f"Started cog successfully!"
