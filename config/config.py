3b37d221886be236424f715219287b69817f7754
Ringo Origo <59732670+RingoOrigo@users.noreply.github.com>
2022-08-30 16:51:06 -0400

    Updated constant variables

--- config/config.py
+++ config/config.py
@@ -1,27 +1,56 @@
 BOT_TOKEN: str = "Place your token here, in these quotes."
 
 TEST_GUILDS = [an, 
-				array, 
-				of, 
-				guid IDs]
+		array, 
+		of, 
+		guid IDs]
 
 STATUS_LIST = ["An", 
-				"Array", 
-				"Of", 
-				"Potential",
-				"Statuses",]
+		"Array", 
+		"Of", 
+		"Potential",
+		"Statuses",]
 
 ROULETTE_DEATH_MESSAGES = ["has blown their own brains out", 
-							"has tripped and fallen onto the trigger", 
-							"won the roulette initially, then got confident and proceeded to try again and blow their own brains out", 
-							"died by their own hand"]
+			"has tripped and fallen onto the trigger", 
+			"won the roulette initially, then got confident and proceeded to try again and blow their own brains out", 
+			"died by their own hand"]
 
 PING_REPLIES = ["Your luck well and truly came to an end on the day you chose to disturb me.", 
-				"Rest assured, your disturbance has not gone unnoticed.", 
-				"You think you can disturb me?", 
-				"I will demonstrate beyond all doubt that there is no future for you in this world.", 
-				"I will end this dream of yours you call reality.", 
-				"Do not provoke me, Ouroboros."]
+		"Rest assured, your disturbance has not gone unnoticed.", 
+		"You think you can disturb me?", 
+		"I will demonstrate beyond all doubt that there is no future for you in this world.", 
+		"I will end this dream of yours you call reality.", 
+		"Do not provoke me, Ouroboros."]
+
+POSITIVE_COMMUNITY_MESSAGES = ["What a good way to spread positivity <3",
+				"How kind of them <3",
+				"The mark of a true friend",
+				"As all good people should do",
+				"A well deserved moment"]
+
+HUG_GIF_LINKS = ["https://media.giphy.com/media/yziFo5qYAOgY8/giphy.gif",
+		"https://media.giphy.com/media/VXP04aclCaUfe/giphy.gif",
+		"https://c.tenor.com/J7eGDvGeP9IAAAAC/enage-kiss-anime-hug.gif",
+		"https://c.tenor.com/Qw4m3inaSZYAAAAC/crying-anime-kyoukai-no-kanata-hug.gif",
+		"https://c.tenor.com/4l0cz9CqXuYAAAAM/girls-hugging-yuri-hug.gif",
+		"https://c.tenor.com/x6EkbdewsTwAAAAM/renora-rwby.gif",
+		"https://c.tenor.com/gqM9rl1GKu8AAAAM/kitsune-upload-hug.gif",
+		"https://c.tenor.com/C7nY0yyhwIIAAAAM/anime.gif",
+		"https://c.tenor.com/ApfJHef4J1UAAAAM/love-anime.gif",
+		"https://c.tenor.com/9SiQQgNwDZcAAAAd/xenoblade-chronicles.gif"]
+
+HEAD_PAT_GIF_LINKS = ["https://c.tenor.com/ROnAfVhg2HIAAAAM/corvin-tora.gif",
+			"https://media.giphy.com/media/ARSp9T7wwxNcs/giphy.gif",
+			"https://media.giphy.com/media/osYdfUptPqV0s/giphy.gif",
+			"https://media.giphy.com/media/109ltuoSQT212w/giphy.gif",
+			"https://media.giphy.com/media/ye7OTQgwmVuVy/giphy.gif",
+			"https://media.giphy.com/media/4HP0ddZnNVvKU/giphy.gif",
+			"https://media.giphy.com/media/e7xQm1dtF9Zni/giphy.gif",
+			"https://c.tenor.com/0sUmFXYB8sMAAAAM/rikkanashii.gif",
+			"https://c.tenor.com/sxoZANeqrT0AAAAM/headpats-anime.gif",
+			"https://c.tenor.com/0gCy2NYphkYAAAAM/anime-pat.gif",
+		      	"https://c.tenor.com/jEUscYib2GkAAAAM/ogiue-chika.gif"]
 
 START_MESSAGE: str = f"Logging in...\nSuccess!\nCalculating version... Done!\nMoebius Version: Alpha 1"
 COG_LOAD: str = f"Loaded cog"
