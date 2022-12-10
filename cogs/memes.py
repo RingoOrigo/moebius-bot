be994b960826c421465b8c4b32beff92b418276c
Ringo Origo <59732670+RingoOrigo@users.noreply.github.com>
2022-12-09 20:01:34 -0500

    Update memes.py
    New command: Check yourself on Moebius' certified Cool Meter

--- cogs/memes.py
+++ cogs/memes.py
@@ -23,5 +23,29 @@ class memes(commands.Cog):
 			return
 		await message.respond(f":boom: {author} " + random.choice(config.ROULETTE_DEATH_MESSAGES) + ", losing the roulette.")
 
+	@commands.slash_command(description="Let Moebius decide if you're cool or not.")
+	async def cool(self, message):
+		coolVal : int = random.randrange(1, 10)
+		result = f"```{message.author.display_name}, on a scale of 1 - 10, you get a {coolVal} on the Moebius Cool Meter```\n**|**"
+		emote = ""
+		
+		if (coolVal < 4):
+			emote = ":red_square:"
+		elif (coolVal < 7):
+			emote = ":yellow_square:"
+		elif (coolVal < 10):
+			emote = ":green_square:"
+		elif (coolVal == 10):
+			emote = ":blue_square"
+
+		i = 1
+		while i<= coolVal:
+			result += emote
+			i = i + 1
+		while coolVal < 10:
+			result += ":black_large_square:"
+			coolVal = coolVal + 1
+		await message.respond(result + "**|**")
+
 def setup(bot):
 	bot.add_cog(memes(bot))
